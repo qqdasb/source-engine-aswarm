c4f3a8015643dd93d7e09ea70f90133e20ffbeee
ihaterockchip <126298565+qqdasb@users.noreply.github.com>
2024-08-16 14:36:26 +0800

    Update build-android-armv8a.sh

--- scripts/build-android-armv8a.sh
+++ scripts/build-android-armv8a.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc -q
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc -q
+# wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc -q
 unzip android-ndk-r10e-linux-x86_64.zip -q
-tar xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+# tar xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 sudo apt update
-sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev
+sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev clang llvm gcc
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
+# export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
 ./waf configure -T release --android=aarch64,host,21 --togles --disable-warns -8 &&
 ./waf build
