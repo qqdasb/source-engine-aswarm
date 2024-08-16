677405aa7adedca0ebf572ec701afaab866d178d
ihaterockchip <126298565+qqdasb@users.noreply.github.com>
2024-08-16 14:44:55 +0800

    Update build-android-armv8a.sh

--- scripts/build-android-armv8a.sh
+++ scripts/build-android-armv8a.sh
@@ -5,7 +5,7 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip -q
 # tar xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 sudo apt update
-sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev clang llvm gcc g++
+sudo apt-get install libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev crossbuild-essential-arm64 build-essential -y --install-recommends
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
 # export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
